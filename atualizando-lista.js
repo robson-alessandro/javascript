16c7849010100cef35ff371a97f8fc52a2e2ab04
robson alessandro <robsonalessandro74@gmail.com>
2023-07-04 20:28:04 -0300

    adicionar arquivo

---
+++ atualizando-lista.js
@@ -0,0 +1,5 @@
+const nomes = ["João", "Ana", "Caio", "Lara", "Marjorie", "Leo"]
+
+nomes.splice(1, 2, "rodrigo")  // aceita tres parametros (onde começa a apagar, quantos elementos apagar, adicionar elemento no lugar)
+
+console.log(nomes)
