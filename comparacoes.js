d59cb82b09bb16dffb364a22cf0d386a55e2b6b4
robson alessandro <robsonalessandro74@gmail.com>
2023-07-04 21:22:21 -0300

    arquivos

---
+++ comparacoes.js
@@ -0,0 +1,20 @@
+// == comparação implicita 
+
+const numero = 5;
+const texto = '5';
+
+// == so compara o valor 
+// === compara o valor e o tipo
+
+
+console.log(numero == texto)
+console.log(numero === texto)
+
+
+// typeof
+console.log(typeof numero)
+console.log(typeof texto)
+
+// metodo explicito de alterar o tipo
+Number()
+String()
